1a56a2f679275ecc232e9f4d1af67d69faded017
Noam Bouillet <noam.bouillet@epitech.eu>
2026-06-02 11:53:46 +0200

    feat: added the encapsulation of poll

---
+++ server/src/Encapsulations/Poll.cpp
@@ -0,0 +1,71 @@
+/*
+** EPITECH PROJECT, 2026
+** Poll
+** File description:
+** Encapsulation of poll
+*/
+
+#include "Poll.hpp"
+#include "ServerExceptions.hpp"
+#include <string>
+
+std::vector<struct pollfd>::iterator Poll::findFd(int fd)
+{
+    std::vector<struct pollfd>::iterator index = _fds.begin();
+
+    while (index != _fds.end()) {
+        if (index->fd == fd)
+            return index;
+        index++;
+    }
+    return _fds.end();
+}
+
+void Poll::addFd(int fd, short events)
+{
+    struct pollfd pollDescriptor;
+
+    if (findFd(fd) != _fds.end())
+        throw ServerException("Poll fd already registered: " + std::to_string(fd) + ".");
+    pollDescriptor.fd = fd;
+    pollDescriptor.events = events;
+    pollDescriptor.revents = 0;
+    _fds.push_back(pollDescriptor);
+}
+
+void Poll::removeFd(int fd)
+{
+    const std::vector<struct pollfd>::iterator index = findFd(fd);
+
+    if (index == _fds.end())
+        throw ServerException("Poll fd not found: " + std::to_string(fd) + ".");
+    _fds.erase(index);
+}
+
+void Poll::setEvents(int fd, short events)
+{
+    const std::vector<struct pollfd>::iterator index = findFd(fd);
+
+    if (index == _fds.end())
+        throw ServerException("Poll fd not found: " + std::to_string(fd) + ".");
+    index->events = events;
+}
+
+int Poll::wait(int timeout)
+{
+    const int result = poll(_fds.data(), _fds.size(), timeout);
+
+    if (result < 0)
+        throw ServerException("poll failed.");
+    return result;
+}
+
+void Poll::clear()
+{
+    _fds.clear();
+}
+
+const std::vector<struct pollfd> &Poll::getFds() const
+{
+    return _fds;
+}
---
+++ server/src/Encapsulations/Poll.hpp
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2026
+** Poll
+** File description:
+** Small wrapper around poll
+*/
+
+#ifndef POLL_HPP_
+#define POLL_HPP_
+
+#include <poll.h>
+#include <vector>
+
+class Poll {
+    private:
+        std::vector<struct pollfd> _fds;
+        std::vector<struct pollfd>::iterator findFd(int fd);
+    public:
+        void addFd(int fd, short events);
+        void removeFd(int fd);
+        void setEvents(int fd, short events);
+        int wait(int timeout = -1);
+        void clear();
+        const std::vector<struct pollfd> &getFds() const;
+};
+
+#endif /* !POLL_HPP_ */
