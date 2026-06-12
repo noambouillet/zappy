c0dc6c5c7f4b76b595d32e459fbbef82dbff8419
louishector <louis.hector@epitech.eu>
2026-06-12 11:32:05 +0200

    feat: do the inventory command first

--- ai/behavior/survive.py
+++ ai/behavior/survive.py
@@ -19,10 +19,10 @@ class Survive(Behavior):
             return ["Look\n"]
         try:
             if ("food" in agent.vision[0]):
-                return ["Take food\n", "Look\n", "Inventory\n"]
+                return ["Take food\n", "Look\n"]
             for pos, infos in enumerate(agent.vision):
                 if ("food" in infos):
-                    return agent.go_to(pos) + ["Take food\n", "Look\n", "Inventory\n"]
+                    return agent.go_to(pos) + ["Take food\n", "Look\n"]
         except (IndexError, TypeError):
             return ["Forward\n"]
         return ["Forward\n", "Look\n"]
