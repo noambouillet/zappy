bf650fd44dec79805d604f113d5593fb9130c32c
louishector <louis.hector@epitech.eu>
2026-06-14 15:01:27 +0200

    feat: the function for check the ressources on the tile of the player

--- ai/behavior/evolution.py
+++ ai/behavior/evolution.py
@@ -6,6 +6,7 @@
 ##
 
 from .class_behavior import Behavior
+from constant import requirement_for_progress
 
 class Evolution(Behavior):
     def execute(self, agent):
@@ -15,5 +16,58 @@ class Evolution(Behavior):
         Returns:
             str: command for the agent
         """
-        return []
+        if (agent.vision == [[]]):
+            return ["Look\n"]
+        requirement = requirement_for_progress[agent.level - 1]
+        nb_players = requirement["nb_players"]
+        action = self.check_ressources_tile(agent, requirement)
+        if (action != ["Look\n"]):
+            return action
+        if (nb_players < self.nb_player_in_tile(agent)):
+            return ["Broadcast"]
+        return ["Ok\n"]
         # return ["Incantation\n"]
+    
+    def nb_player_in_tile(self, agent):
+        """This function is for to count the number of player on the same tile
+        Args:
+            agent (class): Agent/IA
+        """
+        nb_players = 0
+        for elem in agent.vision[0]:
+            if (elem == "player"):
+                nb_players += 1
+        return nb_players
+
+    def check_ressources_tile(self, agent, resources_for_upgrade):
+        """This function is to determinate, the ressources on a tile are necessary/useless 
+        Args:
+            agent (class): Agent/IA
+        """
+        list_command = []
+        print(agent.vision)
+        resources_tiles = {"food" : 0, "linemate" : 0, "deraumere" : 0, "sibur" : 0, "mendiane" : 0, "phiras" : 0, "thystame" : 0}
+        for element in agent.vision[0]:
+            if (element != "player"):
+                resources_tiles[element] += 1
+        #print(resources_tiles)
+        for resource, nb_ressources in resources_tiles.items():
+            if (resource != "nb_players" and resource != "food"):
+                #print(resource, nb_ressources)
+                print(nb_ressources, resources_for_upgrade[resource], resource)
+                if (resource == "food" and nb_ressources > 0):
+                    for _ in range(nb_ressources):
+                        list_command += [f"Take food\n"]
+                    #print(list_command)
+                elif (resource != "food" and nb_ressources > resources_for_upgrade[resource]):
+                    for _ in range(nb_ressources - resources_for_upgrade[resource]):
+                        list_command += [f"Take {resource}\n"]
+                    #print(list_command)
+                elif (resource != "food" and nb_ressources < resources_for_upgrade[resource]):
+                    for _ in range(resources_for_upgrade[resource] - nb_ressources):
+                        list_command += [f"Set {resource}\n"]
+                    #print(list_command)
+        list_command += ["Look\n"]
+        return list_command
+                    
+                    
